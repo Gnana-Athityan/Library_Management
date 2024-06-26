488b29c91ad6ceda3113cdd9f79ee65867f6ed0b
Gnana <125018021@sastra.ac.in>
2024-06-26 14:56:17 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Route,Routes } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Create from './pages/Create.jsx';
+import View from './pages/View.jsx';
+import Delete from './pages/Delete.jsx';
+import Update from './pages/Update.jsx';
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+      <h1>Book Management Store</h1></center>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path= '/create'element={<Create/>}/>
+        <Route path= '/update/:id'element={<Update/>}/>
+        <Route path= '/delete/:id'element={<Delete/>}/>
+      </Routes>
     </div>
-  );
+  )
 }
-
-export default App;
